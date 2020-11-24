5cca24a7e9a404841d83afec8b7e7e4ef416529a
Daniel Ramirez <danric.94m@gmail.com>
2020-11-24 15:10:05 -0600

    Ver respaldoGatto.java

--- src/respaldoGatto.java
+++ src/respaldoGatto.java
@@ -4,29 +4,19 @@ import java.util.Queue;
 
 
 public class Cache {
-	Cycler cycler;
 	DataBlock dataCache[];
 	InstructionBlock instructionCache[];
 	Memory sharedMemory;
 	Messenger messenger;
 	Queue<Integer> indexQueue;
 
-    public Cache(Memory sharedMemory, Messenger messenger, Cycler cycler){
-		this.cycler = cycler;
+    public Cache(Memory sharedMemory, Messenger messenger){
 		dataCache = new DataBlock[4];
-
-
 		instructionCache = new InstructionBlock[8];
-		for(int i = 0; i < instructionCache.length; i++){
-			instructionCache[i] = new InstructionBlock();
-		}
-
 		indexQueue = new LinkedList<>();
 		this.sharedMemory = sharedMemory;
 		this.messenger = messenger;
     }
-
-
 
 /*
 	void cacheMaping(int cacheIndex){
@@ -40,61 +30,45 @@ public class Cache {
 	}
 
 */
-	public void copyWait(){
-		for(int i = 0; i < 4; i++){
-			cycler.nextCycle();
-		}
-	}
 
-	public void copyMemoryWait(){
-		for(int i = 0; i < 24; i++){
-			cycler.nextCycle();
-		}
-	}
-
-	public int[] loadInstruction(int adress){
-		int blockNum = blockAddress(adress, 8);
-		int cacheAdress = blockNum % 8;
-		if((blockNum != instructionCache[cacheAdress].blockNum) || (instructionCache[cacheAdress].status != Block.Status.C)){
-			instructionCache[cacheAdress] = getInstructionBlockFromMemory(adress, blockNum);
-		}
-
-		int wordNum = wordNumber(adress, 8) / 4;
-		return instructionCache[cacheAdress].words[wordNum];
-	}
-
-	public int wordNumber(int adress, int size){
-		return adress % size;
-	}
-
-	public int loadData(){
-		int sopa = 0;
+	public int[] loadInstruction(){
+		int sopa[] = new int[4];
 		return sopa;
+	}
+
+	public void storeInstruction(){
+
+	}
+
+	public int loadData(int memoryAddress){
+		//int dataCache[] = new DataBlock[4];
+
+		int words[2];
+		words[0] = sharedMemory.dataMemory[memoryAddress];	//jalar solo un bloque de memoria
+		words[1] = sharedMemory.dataMemory[memoryAddress+1];
+
+		return dataBlockLoaded;
+
 	}
 
 	public void storeData(){
 
 	}
 
+	void getInstructionBlockFromMemory(int memoryAddress){
+		int initialIndex = blockAddress(memoryAddress, 8) * 8;
+		int word1[];
+		int word2[];
 
-
-	InstructionBlock getInstructionBlockFromMemory(int memoryAddress, int blockNumber){
-		copyMemoryWait();
-		int initialIndex = blockNumber * 8;
-		int words[][] = new int[2][4];
-		for(int i = 0; i < 2; i++){
-			for(int j = 0; j < 4; j++){
-				words[i][j] = sharedMemory.instructionMemory[initialIndex++];
-			}
-		}
-		return new InstructionBlock(blockNumber, words);
+		return new InstructionBlock();
 	}
 
     int blockAddress(int memoryAddress, int blockSize){
       return(memoryAddress / blockSize);
     }
 
-/*
+
+	// storeData
 	void cacheWhenStore(int wordNumber1, int wordNumber2, int blockNumber, boolean failureStatus){
 		if(!failureStatus){
 			this.w1 = wordNumber1;
@@ -119,5 +93,5 @@ public class Cache {
 	boolean getWord(int word){
 		return(true); //(this.__ )
 	}
-*/
+
 }
